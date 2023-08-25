2c979f8aa67d8c7d63dc5e5d63c8be0ef3621f57
Lailatul Karomah <karomahlailatul@gmail.com>
2023-08-25 18:53:50 +0700

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGPASSWORD,
+  password: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+module.exports = pool;
